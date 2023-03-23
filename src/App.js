81324119cc32e541e282afcb0a925d1cef3f5af7
dam150 <116655291+dam150@users.noreply.github.com>
2023-03-23 12:06:09 +0100

    react state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    person: {
+      fullName: "OluDamilola Omo Osinowo",
+      bio: <h4>I am an inspiring web engineer with lots of experience in React, Javascript and Node.Javascript </h4>,
+      imgSrc: "logo.jpg",
+      profession: "Web Developer and Master of Ceremonies"
+    },
+    shows: false,
+    intervalId: null,
+    elapsedTime: 0,
+  };
+
+  componentDidMount() {
+    const intervalId = setInterval(() => {
+      this.setState((prevState) => ({ elapsedTime: prevState.elapsedTime + 1 }));
+    }, 1000);
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  handleToggle = () => {
+    this.setState((prevState) => ({ shows: !prevState.shows }));
+  };
+
+  render() {
+    const { person, shows, elapsedTime } = this.state;
+    return (
+      <div>
+        <button onClick={this.handleToggle}>Toggle</button>
+        {shows && (
+          <div>
+            <img className="img" src={person.imgSrc} alt={person.fullName} />
+            <h1>{person.fullName}</h1>
+            <h2>{person.profession}</h2>
+            <p>{person.bio}</p>
+          </div>
+        )}
+        <p>Elapsed time: {elapsedTime} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
